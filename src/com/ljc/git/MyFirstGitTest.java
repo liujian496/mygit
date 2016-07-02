0d30054aa704b4cd8f335b11e849f3892e204436
liujian496 <liujian496@163.com>
2016-07-02 14:24:57 +0800

    my test project

---
+++ src/com/ljc/git/MyFirstGitTest.java
@@ -0,0 +1,10 @@
+package com.ljc.git;
+
+/**
+ * Created by LIUJIAN on 2016/7/2.
+ */
+public class MyFirstGitTest {
+    public static void main(String[] args) throws Exception {
+        System.out.println("abcdefg");
+    }
+}
